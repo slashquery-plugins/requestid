f8b4a9ddb6534730716b91903e25f75fdda9677f
nbari <nbari@tequila.io>
2017-06-22 17:58:04 +0200

    modified:   main.go

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package requestid
 
 import (
-	"crypto/rand"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func RequestID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rid := r.Header.Get("X-Request-ID")
 		if rid == "" {
-			rid, err := newUUID()
+			// Creating UUID Version 1
+			uuid1 := uuid.NewV1()
+			rid, err := uuid1
 			if err != nil {
 				log.Println(err)
 			}
@@ -20,18 +21,4 @@ func RequestID(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
-}
-
-// newUUID generates a random UUID according to RFC 4122
-func newUUID() (string, error) {
-	uuid := make([]byte, 16)
-	n, err := io.ReadFull(rand.Reader, uuid)
-	if n != len(uuid) || err != nil {
-		return "", err
-	}
-	// variant bits; see section 4.1.1
-	uuid[8] = uuid[8]&^0xc0 | 0x80
-	// version 4 (pseudo-random); see section 4.1.3
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
